f9360112620bf1c33e845c7163095a6c8f041421
TheFadGhost <TheFadGhost@users.noreply.github.com>
2026-08-23 19:12:23 +0100

    Release 1.0.0

--- logsift/__init__.py
+++ logsift/__init__.py
@@ -1,5 +1,6 @@
 """Logsift: streaming log analyzer with explainable anomaly detection."""
 
-__version__ = "0.1.0"
+__version__ = "1.0.0"
 
 from .clock import Clock, FakeClock, SystemClock  # noqa: F401
+
